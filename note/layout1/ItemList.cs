f2dca5eb55c777fbb4e3261c246767ac32bf0986
xf <lxf0525@gmail.com`>
2015-03-31 21:52:25 +0800

    more

---
+++ note/layout1/ItemList.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace layout1
+{
+
+    class Item
+    {
+        public string desc
+        {
+            get;
+            set;
+        }
+        public bool isChecked = false;
+    }
+
+    
+
+    class ItemList
+    {
+        public string title = "title";
+        public bool achived = false;
+        public List<Item> mItems = new List<Item>();
+
+        public Item addItem()
+        {
+            var item = new Item();
+            mItems.Add(item);
+            return item;
+        }
+
+        public void rmItem(Item item)
+        {
+            mItems.Remove(item);
+        }
+
+        public void changePosBefore(Item item)
+        {
+            var oldIdx = mItems.IndexOf(item);
+            var newIdx = oldIdx - 1;
+            if(newIdx<0)
+            {
+                newIdx = 0;
+            }
+            mItems.Remove(item);
+            mItems.Insert(newIdx, item);
+        }
+
+        public void changePosAfter(Item item, Item item2)
+        {
+            var oldIdx = mItems.IndexOf(item);
+            var newIdx = oldIdx;
+            if (newIdx < 0)
+            {
+                newIdx = 0;
+            }
+            mItems.Remove(item);
+            mItems.Insert(newIdx, item);
+        }
+    }
+
+    class ItemNote
+    {
+        public List<ItemList> mItemLists = new List<ItemList>();
+    }
+}
